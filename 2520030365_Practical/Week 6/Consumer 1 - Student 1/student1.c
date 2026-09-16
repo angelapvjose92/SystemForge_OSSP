a75f2c2765cf129377ed00aa24afaf61ce945375
Angela <angelapvjose92@gmail.com>
2026-09-16 09:10:55 +0530

    Add files via upload

---
+++ 2520030365_Practical/Week 6/Consumer 1 - Student 1/student1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#define SUBJECTS 5
+
+int main()
+{
+    const char *shm_name = "/student1_marks";
+    int fd;
+    int *marks;
+    int total = 0;
+
+    fd = shm_open(shm_name, O_RDONLY, 0666);
+
+    if (fd == -1)
+    {
+        perror("shm_open");
+        return 1;
+    }
+
+    marks = mmap(NULL, SUBJECTS * sizeof(int),
+                 PROT_READ, MAP_SHARED, fd, 0);
+
+    if (marks == MAP_FAILED)
+    {
+        perror("mmap");
+        return 1;
+    }
+
+    printf("\nStudent 1\n");
+    printf("--------------------------------\n");
+
+    for (int i = 0; i < SUBJECTS; i++)
+    {
+        printf("Subject %d : %d\n", i + 1, marks[i]);
+        total += marks[i];
+    }
+
+    printf("\nTotal : %d\n", total);
+    printf("Average : %.2f\n", total / 5.0);
+
+    munmap(marks, SUBJECTS * sizeof(int));
+    close(fd);
+
+    return 0;
+}
